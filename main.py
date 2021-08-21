9f361a7e7cce2f33c9aa50084582bfa863219593
ComicoTeam <comico_work@outlook.com>
2021-08-21 21:47:53 +0300

    Show the current CPU TEMP

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import urllib2
 from inky import InkyPHAT
 from PIL import Image, ImageFont, ImageDraw
 from font_fredoka_one import FredokaOne
+from gpiozero import CPUTemperature
 
 # Set current directory
 
@@ -31,12 +32,15 @@ except:
 
 font = ImageFont.truetype(FredokaOne, 16)
 
+cputemp = CPUTemperature().temperature
+
 inky_display = InkyPHAT("red")
 inky_display.set_border(inky_display.WHITE)
 
 draw.text((10,15), str(adsblocked) + " AD'S", inky_display.RED, font)
 draw.text((10,35), str("%.1f" % round(ratioblocked,2)) + " AD%", inky_display.RED, font)
 draw.text((10,55), str(dnsqueries) + " DNS Req", inky_display.RED, font)
+draw.text((10,85), str(round(cputemp)) + " *C", inky_display.BLACK, font)
 
 inky_display.set_image(img)
 
